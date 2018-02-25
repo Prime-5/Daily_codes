e6e73c020699ca6d1400d5df9866f1cc497af562
Abhishek Pratap Singh <35230163+Prime-5@users.noreply.github.com>
2018-02-25 10:02:52 +0530

    Update bubble_sort_abhi.cpp
    Code for Selection Sort in C++

--- bubble_sort_abhi.cpp
+++ bubble_sort_abhi.cpp
@@ -1,1 +1,43 @@
-Testing purposes.
+#include<iostream>
+using namespace std;
+
+void selectionsort(int n,int *a)
+{
+	int i,j,temp;
+	for(i=0;i<n-1;i++)
+	{	for(j=0;j<n-i-1;j++)
+		{	if(a[j]>a[j+1])
+			{	temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n,i;
+	cout<<"Enter size of array"<<endl;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+	{	cin>>a[i];
+	}
+
+	cout<<"\nOriginal array"<<endl;
+	for(i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+
+	selectionsort(n,a);
+
+	cout<<"\nSorted Array"<<endl;
+	for(i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+
+	return 0;
+}
