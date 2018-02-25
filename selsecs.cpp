b769a8ff5fe3c1a939a7a93ac69575aeb466fc4f
calra123 <32410407+calra123@users.noreply.github.com>
2018-02-24 20:50:48 -0800

    Add files via upload

---
+++ selsecs.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int selections(int n,int *ar)
+{
+ int i;
+ int j;
+ int mini;
+ int pos;
+ int temp;
+ int l;
+ for(j=0;j<n;j++)
+{
+ 
+	mini=ar[j];
+	pos=j;
+	for(i=j;i<n-1;i++)
+	{
+	 if(mini>ar[i+1])
+	  { pos=i+1;
+       	    mini=ar[i+1];
+	  }
+	}
+
+ temp=ar[j];
+ ar[j]=mini;
+ ar[pos]=temp;
+}
+	for(l=0;l<n;l++)
+	cout<<ar[l]<<endl;
+
+}
+
+
+
+
+int main()
+{
+ int n;
+ cin>>n;
+ int ar[n];
+ for(int i=0;i<n;i++)
+ cin>>ar[i];
+ selections(n,ar);
+}
