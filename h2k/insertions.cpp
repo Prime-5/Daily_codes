0ef89250a08b9f57f9ca3d8f07d7db35dec6e197
calra123 <32410407+calra123@users.noreply.github.com>
2018-02-27 19:02:20 -0800

    Add files via upload
    insertion sort added

---
+++ h2k/insertions.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int insertions(int n,int*a)
+{
+ int i,temp,j,key;
+ for(i=0;i<n;i++)
+ {
+ key=a[i];
+ j=i-1;
+ 	while(j>=0&& key<a[j])
+	{ 
+  	a[j+1]=a[j];
+  	j--;
+	}
+
+a[j+1]=key;
+}
+for(i=0;i<n;i++)
+cout<<a[i]<<endl;	
+}
+
+int main()
+{
+ int n;
+ int i;
+ cin>>n;
+ int ar[n];
+ for(i=0;i<n;i++)
+ cin>>ar[i];
+ insertions(n,ar);
+}
