9ddc1fd9c17add9eb5a9b987a4de9c9e6eea891b
calra123 <32410407+calra123@users.noreply.github.com>
2018-03-03 11:12:35 -0800

    uploaded quicksort

---
+++ h2k/quicksort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+void swap(int &a,int &b)
+{
+ int temp;
+ temp=a;
+ a=b;
+ b=temp;
+}
+
+int partition(int p,int r, int *ar)
+{
+ int i,j;
+ i=p-1;
+ j=p;
+ for(j=p;j<r;j++)
+{
+  if(ar[j]<ar[r])
+ { 
+   i=i+1;
+   swap(ar[i],ar[j]);
+ } 
+ 
+}
+swap(ar[i+1],ar[j]);
+return i+1;
+}
+
+void quicksort(int p,int r,int *ar)
+{
+ int q;
+ if(p<r)
+ {q=partition(p,r,ar);
+ quicksort(p,q-1,ar);
+ quicksort(q+1,r,ar);
+}
+}
+int main()
+{
+ int n;
+ int i;
+ int r;
+ cin>>n;
+ int p=0;
+ r=n-1;
+ int ar[n];
+ for(i=0;i<n;i++)
+ cin>>ar[i];
+ quicksort(p,r,ar);
+ for(i=0;i<n;i++)
+ cout<<ar[i]<<endl;
+ 
+}
+
